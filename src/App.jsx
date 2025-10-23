5658feeb883de4bde1e30a8d75f4596290f9960c
basswoman <54603761+basswoman@users.noreply.github.com>
2025-10-23 10:41:30 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Esper Audio</h1>
+
+      <p className="read-the-docs">
+        I'm building this website. Wait another few years
+      </p>
+    </>
+  );
+}
+
+export default App;
